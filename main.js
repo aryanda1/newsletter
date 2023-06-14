6c96e3f9a2d595d3ba272ec7c0dd68b414b6a4ac
Aryan Kumar <lci2020004@iiitl.ac.in>
2023-06-14 15:20:28 +0530

    Added animation and updated some styles.

--- main.js
+++ main.js
@@ -3,14 +3,19 @@ function submitForm(event) {
   const email = document.getElementById("email").value;
   var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   console.log(email);
-  if(!email || !emailRegex.test(email)){
+  if (!email || !emailRegex.test(email)) {
     error_label.style.display = "inline";
     email_id.classList.add("error");
     return;
   }
   myForm.reset();
-  container.style.display = "none";
-  sucess_container.style.display = "flex";
+  container.classList.remove("active");
+  container.classList.add("hidden");
+  // sucess_container.style.display = "flex";
+  sucess_container.classList.remove("hidden");
+  setTimeout(() => {
+    sucess_container.classList.add("active");
+  }, 300);
   sucess_message.innerHTML = `A confirmation email has been sent to <b>${email}</b>. Please open it and click the button inside to confirm your subscription.`;
 }
 
@@ -21,7 +26,11 @@ const container = document.getElementsByClassName("container")[0];
 
 const sucess_container = document.getElementsByClassName("success-cont")[0];
 
-function dismiss(){
-  sucess_container.style.display = "none";
-  container.style.display = "grid";
+function dismiss() {
+  // sucess_container.style.display = "none";
+  sucess_container.classList.remove("active");
+  sucess_container.classList.add("hidden");
+  // container.style.display = "grid";
+  container.classList.remove("hidden");
+  setTimeout(() => container.classList.add("active"), 300);
 }
